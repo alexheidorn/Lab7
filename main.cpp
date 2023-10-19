7ca40047e603118afe792dbaee100ed99c695c35
alexheidorn <alexheidorn12@gmail.com>
2023-10-19 14:03:15 -0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,87 @@
+/*
+* Alex Heidorn
+* Created: 10-19-23
+* CS2 Lab7
+*  
+*/
+
+#include <iostream>
+#include <list>
+#include <stdlib.h>
+#include <vector>
+#include <queue>
+using namespace std;
+
+class PlayingCard {
+private:
+    int suitNo;
+    int cardNo;
+public:
+    friend bool operator<(const PlayingCard &card1, const PlayingCard &card2) {
+        return ((card1.cardNo < card2.cardNo) ||
+                ((card1.cardNo == card2.cardNo) && (card1.suitNo < card2.suitNo)));
+    }
+    PlayingCard(int n, int s) { cardNo = n; suitNo = s; }
+    static const string FULLSUITS[4];
+    static const string SHORTSUITS[4];
+    static const string CARDNAME[13];
+    string shortCardName() { return CARDNAME[cardNo] + SHORTSUITS[suitNo]; }
+    string longCardName() { return CARDNAME[cardNo] + FULLSUITS[suitNo]; }
+};
+
+const string PlayingCard::FULLSUITS[] = {"Clubs", "Diamonds", "Hearts", "Spades"};
+const string PlayingCard::SHORTSUITS[] = {"C", "D", "H", "S"};
+const string PlayingCard::CARDNAME[] = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+
+
+int RandInRange(int low, int high) {
+    return (low + rand()%(high - low + 1));
+}
+
+list<PlayingCard> generateShuffledDeck() {
+    vector<PlayingCard> allCards;
+    list<PlayingCard> shuffledCards;
+    srand(time(0));
+    for (int n = 0; n < 13; n++) {
+        for (int s = 0; s < 4; s++) {
+            allCards.push_back(PlayingCard(n,s));
+        }
+    }
+    // for (auto c : allCards) {
+    //    cout << c.shortCardName() << " " << endl;
+    // }
+    for (int i = 51; i >= 0; i--) {
+        int choice = RandInRange(0,i);
+        shuffledCards.push_back(allCards[choice]);
+        allCards[choice] = allCards[i];
+        allCards.pop_back();
+    }
+    return shuffledCards;
+}
+
+void dealCards(list<PlayingCard> cards, queue<PlayingCard> & p1queue, queue<PlayingCard> & p2queue) {
+    while (!cards.empty()) {
+        PlayingCard card = cards.front();
+        cards.pop_front();
+        p1queue.push(card);
+        if (cards.empty()) {
+            cerr << "WARNING! Uneven number of cards\n";
+        }
+        else {
+            card = cards.front();
+            cards.pop_front();
+            p2queue.push(card);
+        }
+    }
+
+}
+
+int main() {
+    list<PlayingCard> cards = generateShuffledDeck();
+    queue<PlayingCard> p1queue;
+    queue<PlayingCard> p2queue;
+    dealCards(cards,p1queue,p2queue);
+    int rounds = 0;
+    // Play the game until one players wins and report how many rounds that took
+    return 0;
+}
